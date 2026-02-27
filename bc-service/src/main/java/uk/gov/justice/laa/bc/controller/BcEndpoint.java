8f2edc9998d40469715a8d5c5f7922bacae2a247
savvysoftben <ben@savvysoftware.com>
2026-02-27 11:57:03 +0000

    first cut SOAP
    # Conflicts:
    #	README.md
    #	bc-service/build.gradle
    #	bc-service/request.xml
    #	bc-service/src/main/java/uk/gov/justice/laa/bc/config/WebServiceConfig.java
    #	bc-service/src/main/resources/META-INF/schemas/LSC_BenefitChecker_WS_01.xsd

---
+++ bc-service/src/main/java/uk/gov/justice/laa/bc/controller/BcEndpoint.java
@@ -0,0 +1,24 @@
+package uk.gov.justice.laa.bc.controller;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import uk.gov.lsc.benefitchecker.data._1.BenefitCheckerRequest;
+import uk.gov.lsc.benefitchecker.data._1.BenefitCheckerResponse;
+
+/**
+ * Service to receive SOAP request in a modern JDK 25 / Spring Boot 4 way
+ */
+@Endpoint
+public class BcEndpoint {
+
+  public static final String LOCAL_PART = "check";
+  public static final String NAMESPACE_URI = "https://lsc.gov.uk/benefitchecker/service/1.0/API_1.0_Check";
+
+  @PayloadRoot(namespace = NAMESPACE_URI, localPart = LOCAL_PART)
+  @ResponsePayload
+  public BenefitCheckerResponse performBenefitsCheck(@RequestPayload BenefitCheckerRequest request) {
+    return new BenefitCheckerResponse();
+  }
+}
