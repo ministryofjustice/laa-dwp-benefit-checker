b7ad1bede482fb4af113fb5a259dcc49130d0606
savvysoftben <ben@savvysoftware.com>
2026-02-27 13:07:12 +0000

    first cut SOAP
    # Conflicts:
    #	bc-service/src/main/java/uk/gov/justice/laa/bc/endpoint/BenefitCheckerController.java

--- bc-service/src/main/java/uk/gov/justice/laa/bc/endpoint/BcEndpoint.java
+++ bc-service/src/main/java/uk/gov/justice/laa/bc/endpoint/BcEndpoint.java
@@ -1,5 +1,7 @@
-package uk.gov.justice.laa.bc.controller;
+package uk.gov.justice.laa.bc.endpoint;
 
+import jakarta.xml.bind.JAXBElement;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -7,9 +9,12 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import uk.gov.lsc.benefitchecker.data._1.BenefitCheckerRequest;
 import uk.gov.lsc.benefitchecker.data._1.BenefitCheckerResponse;
 
+import javax.xml.namespace.QName;
+
 /**
  * Service to receive SOAP request in a modern JDK 25 / Spring Boot 4 way
  */
+@Slf4j
 @Endpoint
 public class BcEndpoint {
 
@@ -18,7 +23,12 @@ public class BcEndpoint {
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = LOCAL_PART)
   @ResponsePayload
-  public BenefitCheckerResponse performBenefitsCheck(@RequestPayload BenefitCheckerRequest request) {
-    return new BenefitCheckerResponse();
+  public JAXBElement<BenefitCheckerResponse> check(@RequestPayload JAXBElement<BenefitCheckerRequest> request) {
+    log.info("Got request {}", request.getValue());
+    BenefitCheckerResponse response = new BenefitCheckerResponse();
+    response.setBenefitCheckerStatus("No");
+    response.setConfirmationRef("a ref");
+    response.setOriginalClientRef("orig");
+    return new JAXBElement<>(new QName("benefitCheckerResponse"), BenefitCheckerResponse.class, response);
   }
 }
