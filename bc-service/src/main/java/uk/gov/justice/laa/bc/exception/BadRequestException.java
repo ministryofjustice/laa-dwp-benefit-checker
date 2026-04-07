b2796cc7dfc765f9c92f84005ac8bd8a49ade5b6
savvysoftben <ben@savvysoftware.com>
2026-04-07 10:44:42 +0100

    add in dependency to deploy-uat to force an ordered deployment.  ie. if deploy-uat fails, we don't want to attempt prod deploy

--- bc-service/src/main/java/uk/gov/justice/laa/bc/exception/BadRequestException.java
+++ bc-service/src/main/java/uk/gov/justice/laa/bc/exception/BadRequestException.java
@@ -1,6 +1,10 @@
 package uk.gov.justice.laa.bc.exception;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
